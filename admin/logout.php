772f5036a825cf56ee80c6e15e3835f9f1c33bad
MoeJoeMJ <manoj612kumar@gmail.com>
2023-03-02 22:31:27 +0530

    files deleted

--- admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['usertype']);
-	unset($_SESSION['userid']);
-	header('Location: ../index.php');
-?>
--- admin/showerror.php
+++
@@ -1,6 +0,0 @@
-<div class="alert alert-danger alert-dismissible" role="alert" style="margin-top:20px;">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <?php
-	echo $errinfo;  
-  ?>
-</div>
